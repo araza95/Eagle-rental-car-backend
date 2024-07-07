c95bd0be2c96b7bec5cbb4776dff5a42cf817378
araza95 <alihaiderizvi.you@gmail.com>
2024-07-07 05:42:28 +0500

    `Feat: ` - payment controller added.

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,1 +1,15 @@
 // Controller
+import { Request, Response } from "express";
+import { processPayment } from "../services/payment.service";
+
+export const handlePayment = async (req: Request, res: Response) => {
+  const { clientEmail, adminEmail, subject, message } = req.body;
+
+  try {
+    const paymentInfo = { clientEmail, adminEmail, subject, message };
+    const result = await processPayment({ paymentInfo });
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
