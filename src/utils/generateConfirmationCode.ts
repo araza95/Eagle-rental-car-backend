cc5579e9d57b0b697914d24e4530bc3352258fd6
araza95 <alihaiderizvi.you@gmail.com>
2024-07-07 08:33:04 +0500

    `Feat:  ` - confirmation code generator util added.

---
+++ src/utils/generateConfirmationCode.ts
@@ -0,0 +1,17 @@
+function generateConfirmationCode(): string {
+  const characters: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const length: number = 6; // Adjust the length of the code as needed
+  let code: string = "ECR";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex: number = Math.floor(Math.random() * characters.length);
+    code += characters[randomIndex];
+  }
+
+  return code;
+}
+
+// Generate a random confirmation code
+const confirmationCode: string = generateConfirmationCode();
+
+console.log(confirmationCode);
