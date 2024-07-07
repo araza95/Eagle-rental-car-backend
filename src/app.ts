3ab5d2efa7c50f3fda8d3e33dd2ab8d67ea19e51
araza95 <alihaiderizvi.you@gmail.com>
2024-07-07 06:12:28 +0500

    Update app.ts to include Request and Response types

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import paymentRoutes from "./routes/paymentRoutes";
 
 const app = express();
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use("/api", paymentRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world EaglePay!");
 });
 
 export default app;
