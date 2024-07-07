0f1cc4ce630ecc1dead44966ea1b29709e301aa8
araza95 <alihaiderizvi.you@gmail.com>
2024-07-07 05:28:14 +0500

    Add nodemailer for sending emails

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,1 +1,33 @@
-// mail service
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendMail = async ({
+  to,
+  subject,
+  html,
+}: {
+  to: string;
+  subject: string;
+  html: string;
+}): Promise<void> => {
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text: html,
+    });
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
