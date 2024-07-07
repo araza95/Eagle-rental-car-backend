ada0f54c3d879311ab0ddcd57ddfc0767bc20036
araza95 <alihaiderizvi.you@gmail.com>
2024-07-07 05:26:04 +0500

    `Feat: ` - used best practice - Folder structure updated.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
-import express, { Request, Response } from "express";
-import Stripe from "stripe";
-import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import express, { Request, Response } from "express";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const stripe = new Stripe("your-stripe-secret-key", {
-  apiVersion: "2024-06-20",
-});
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "your-email@gmail.com",
-    pass: "your-email-password",
-  },
-});
 
 app.use(express.json());
 
